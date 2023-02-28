3d7fb17a1a4959d804bd2d2037f4fc670a321bb6
igdmedvedev <120250210+igdmedvedev@users.noreply.github.com>
2023-02-28 11:06:24 +0500

    Добавлена кнопка для начала новой игры. После конца игры блокируется отправка формы и поле ввода. Добавлен фокус для начала и конца игры.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
-const min = 10, max = 100;
+const min = 100, max = 140;
 const prNum = Math.floor(Math.random() * (max - min) + min);
-let attempts = Math.round(Math.log2(max - min + 1));
+let attempts = Math.ceil(Math.log2(max - min + 1));
 let flagWin = false;
 
 document.getElementById('attempts').innerHTML = "Attempts: " + attempts;
@@ -8,6 +8,7 @@ document.getElementById('attempts').innerHTML = "Attempts: " + attempts;
 let inputNum = document.getElementById('input-num');
 inputNum.min = min;
 inputNum.max = max;
+inputNum.focus();
 
 let out = document.getElementById('out');
 
@@ -16,27 +17,40 @@ document.getElementById('form').onsubmit = function () {
         return false;
     }
 
-    const num = inputNum.value;
     if (attempts > 0) {
         document.getElementById('attempts').innerHTML = "Attempts: " + (attempts -= 1);
 
+        const num = inputNum.value;
         if (num == prNum) {
             out.innerHTML = "You won!";
             flagWin = true;
+            disableGame();
         } else if (attempts === 0) {
             out.innerHTML = "You lose";
+            disableGame();
         } else if (num > prNum) {
             out.innerHTML = "Less";
         } else if (num < prNum) {
             out.innerHTML = "More";
         } else {
             out.innerHTML = "Error";
+            disableGame();
         }
     }
     return false;
 }
 
-document.getElementById('outNum').innerHTML = prNum;
+function disableGame() {
+    document.getElementById('input-num').disabled = true;
+    document.getElementById('guess-btn').disabled = true;
+    document.getElementById('new-game-btn').focus();
+}
+
+document.getElementById('new-game-btn').onclick = function () {
+    location.reload();
+}
+
+//document.getElementById('outNum').innerHTML = prNum;
 document.getElementById('min').innerHTML = min;
 document.getElementById('max').innerHTML = max;
 document.getElementById('attempts1').innerHTML = attempts;
